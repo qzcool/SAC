b6a5613e6afbdf202e532b04de599670d6ab2edb
Qiao Zhang <qiao.zhang.qz@outlook.com>
2018-09-26 14:48:12 +0800

    Improvement

--- scraper.py
+++ scraper.py
@@ -13,12 +13,15 @@ class Company():
     def __init__(self):
         self.url_company_list = 'http://person.sac.net.cn/pages/registration/sac-publicity-report.html'
 
+        option = webdriver.ChromeOptions()
+        option.add_argument('headless')
+        self.driver = webdriver.Chrome(chrome_options=option)
+
     # 获取所有证券公司名单
-    def get_company_list(self):
-        driver = webdriver.Chrome()
-        driver.get(self.url_company_list)
+    def get_company_list(self, save_to_local=True):
+        self.driver.get(self.url_company_list)
         time.sleep(8)  # 确保网页完全加载
-        content = driver.page_source.encode('utf-8')
+        content = self.driver.page_source.encode('utf-8')
         soup = BeautifulSoup(content, 'lxml')
 
         table = soup.find('div', align='center').find_all('table')[-1]
@@ -44,24 +47,26 @@ class Company():
 
         # 描述表格数据
         print('表格共有{}列，{}行。'.format(len(df_companies.columns), len(df_companies)))
-        driver.quit()
+        self.driver.quit()
         display(df_companies)
+
+        # 本地保存人员列表
+        if save_to_local:
+            df_companies.to_csv('列表_证券公司.csv', encoding='gb18030')
 
         return df_companies
 
-# TODO: 是否要设置People和Company类别的继承关系：考虑可能
+# TODO: 是否要设置People和Company类别的继承关系：考虑可能采用分布式加快速度
 class People(Company):
     def __init__(self):
-        pass
-        # super.
+        super().__init__()
         # self.url_people_list = 'http://person.sac.net.cn/pages/registration/sac-publicity-finish.html?aoiId='
 
     # 获取单个公司的所有员工名单
     def get_people_list(self, company_ID):
         # 启动浏览器
         # TODO：添加Headless选项
-        driver = webdriver.Chrome()
-        driver.get('http://person.sac.net.cn/pages/registration/sac-publicity-finish.html?aoiId=' + company_ID)
+        self.driver.get('http://person.sac.net.cn/pages/registration/sac-publicity-finish.html?aoiId=' + company_ID)
         time.sleep(2)  # Driver加载完如果直接取数最好设置延迟
 
         # 确认页数
@@ -99,15 +104,16 @@ class People(Company):
             return data, list_columns
 
         # 主程序部分
-        pages_num = get_pages_num(driver)
+        pages_num = get_pages_num(self.driver)
 
+        # TODO：性能提升-使用deque代替list
         data = []
-        data, list_columns = parser(driver, data)
+        data, list_columns = parser(self.driver, data)
         for i in tqdm(range(pages_num - 1)):  ## 最后一页不用点，可以换成while循环更优雅
-            change_page(driver)
-            data, list_columns = parser(driver, data)
+            change_page(self.driver)
+            data, list_columns = parser(self.driver, data)
 
-        driver.quit()
+        self.driver.quit()
         df_people = pd.DataFrame(data, columns=list_columns)
 
         # 描述表格数据
@@ -115,9 +121,39 @@ class People(Company):
         display(df_people)
         return df_people
 
+    # 获取单个员工的照片路径：person_ID即为PPP_ID
+    def get_person_info_img(self, person_ID):
+        self.driver.get('http://person.sac.net.cn/pages/registration/sac-finish-person.html?r2SS_IFjjk=' + person_ID)  # '76B6EA9E1C873C0DE053D651A8C06CD1')
+        return self.driver.find_elements_by_tag_name('img')[-1].get_attribute("src")
+
     # 获取单个员工的所有个人信息
-    def get_people_info(self, person_ID):
+    # TODO: 需要挖掘每个人的信息，尽量使用Requests提高速度
+    def get_person_info(self, person_ID):
+        # 获取单个员工的照片路径
+        #url_img = self.get_person_info_img(person_ID)
+        # 获取注册变更记录
+
         pass
 
     # 批量获取所有公司的所有人的信息
     # TODO：需要使用分布式爬虫来提高速度
+    def get_people_list_full(self, save_to_local=True):
+        # 获取所有公司列表
+        df_companies = self.get_company_list()
+
+        # 传入所有公司的机构ID，循环爬取
+        df_people_full = pd.DataFrame()
+
+        for i in tqdm(df_companies.机构ID):
+            df_people = People().get_people_list(company_ID=i)
+            df_people_full = df_people_full.append(df_people)
+
+        # 描述表格数据
+        print('表格共有{}列，{}行。'.format(len(df_people_full.columns), len(df_people_full)))
+        display(df_people_full)
+
+        # 本地保存人员列表
+        if save_to_local:
+            df_people_full.to_csv('列表_从业人员.csv', encoding='gb18030')
+
+        return df_people_full
